1cacdb852d15430802735aef4b906de69c4d30c9
haptome <haptome2@icloud.com>
2021-07-03 14:19:49 +0300

    Update dot_navigation_bar.dart
    new change

--- lib/dot_navigation_bar.dart
+++ lib/dot_navigation_bar.dart
@@ -18,7 +18,7 @@ class DotNavigationBar extends StatelessWidget {
     this.marginR = const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
     this.paddingR = const EdgeInsets.only(bottom: 5, top: 10),
     this.borderRadius = 30,
-    this.backgroundColor =  Colors.white,
+    this.backgroundColor = Colors.white,
   }) : super(key: key);
 
   /// A list of tabs to display, ie `Home`, `Profile`,`Cart`, etc
@@ -53,14 +53,15 @@ class DotNavigationBar extends StatelessWidget {
 
   /// margin for the bar to give some radius
   final EdgeInsetsGeometry? marginR;
+
   /// padding for the bar to give some radius
   final EdgeInsetsGeometry? paddingR;
+
   /// border radius
   final double? borderRadius;
+
   ///bgd colors for the nav bar
   final Color? backgroundColor;
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -83,99 +84,94 @@ class DotNavigationBar extends StatelessWidget {
               width: double.infinity,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8),
-                child:  Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              for (final item in items)
-                TweenAnimationBuilder<double>(
-                  tween: Tween(
-                    end: items.indexOf(item) == currentIndex ? 1.0 : 0.0,
-                  ),
-                  curve: curve,
-                  duration: duration,
-                  builder: (context, t, _) {
-                    final _selectedColor = item.selectedColor ??
-                        selectedItemColor ??
-                        theme.primaryColor;
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    for (final item in items)
+                      TweenAnimationBuilder<double>(
+                        tween: Tween(
+                          end: items.indexOf(item) == currentIndex ? 1.0 : 0.0,
+                        ),
+                        curve: curve,
+                        duration: duration,
+                        builder: (context, t, _) {
+                          final _selectedColor = item.selectedColor ??
+                              selectedItemColor ??
+                              theme.primaryColor;
 
-                    final _unselectedColor = item.unselectedColor ??
-                        unselectedItemColor ??
-                        theme.iconTheme.color;
+                          final _unselectedColor = item.unselectedColor ??
+                              unselectedItemColor ??
+                              theme.iconTheme.color;
 
-                    return Material(
-                      color:
-                          Color.lerp(Colors.transparent, Colors.transparent, t),
-                      child: InkWell(
-                        onTap: () => onTap?.call(items.indexOf(item)),
-                        focusColor: _selectedColor.withOpacity(0.1),
-                        highlightColor: _selectedColor.withOpacity(0.1),
-                        splashColor: _selectedColor.withOpacity(0.1),
-                        hoverColor: _selectedColor.withOpacity(0.1),
-                        child: Stack(children: <Widget>[
-                          Padding(
-                            padding: itemPadding -
-                                EdgeInsets.only(right: itemPadding.right * t),
-                            child: Row(
-                              children: [
-                                IconTheme(
-                                  data: IconThemeData(
-                                    color: Color.lerp(
-                                        _unselectedColor, _selectedColor, t),
-                                    size: 24,
+                          return Material(
+                            color: Color.lerp(
+                                Colors.transparent, Colors.transparent, t),
+                            child: InkWell(
+                              onTap: () => onTap?.call(items.indexOf(item)),
+                              focusColor: _selectedColor.withOpacity(0.1),
+                              highlightColor: _selectedColor.withOpacity(0.1),
+                              splashColor: _selectedColor.withOpacity(0.1),
+                              hoverColor: _selectedColor.withOpacity(0.1),
+                              child: Stack(children: <Widget>[
+                                Padding(
+                                  padding: itemPadding -
+                                      EdgeInsets.only(
+                                          right: itemPadding.right * t),
+                                  child: Row(
+                                    children: [
+                                      IconTheme(
+                                        data: IconThemeData(
+                                          color: Color.lerp(_unselectedColor,
+                                              _selectedColor, t),
+                                          size: 24,
+                                        ),
+                                        child: item.icon,
+                                      ),
+                                    ],
                                   ),
-                                  child: item.icon,
                                 ),
-                              ],
-                            ),
-                          ),
-                          ClipRect(
-                            child: SizedBox(
-                              height: 40,
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                widthFactor: t,
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                      left: itemPadding.right / 0.63,
-                                      right: itemPadding.right),
-                                  child: DefaultTextStyle(
-                                    style: TextStyle(
-                                      color: Color.lerp(
-                                          _selectedColor.withOpacity(0.0),
-                                          _selectedColor,
-                                          t),
-                                      fontWeight: FontWeight.w600,
+                                ClipRect(
+                                  child: SizedBox(
+                                    height: 40,
+                                    child: Align(
+                                      alignment: Alignment.bottomCenter,
+                                      widthFactor: t,
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                            left: itemPadding.right / 0.63,
+                                            right: itemPadding.right),
+                                        child: DefaultTextStyle(
+                                          style: TextStyle(
+                                            color: Color.lerp(
+                                                _selectedColor.withOpacity(0.0),
+                                                _selectedColor,
+                                                t),
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                          child: CircleAvatar(
+                                              radius: 2.5,
+                                              backgroundColor:
+                                                  dotIndicatorColor != null
+                                                      ? dotIndicatorColor
+                                                      : _selectedColor),
+                                        ),
+                                      ),
                                     ),
-                                    child: CircleAvatar(
-                                        radius: 2.5,
-                                        backgroundColor: dotIndicatorColor != null
-                                            ? dotIndicatorColor
-                                            : _selectedColor),
                                   ),
                                 ),
-                              ),
+                              ]),
                             ),
-                          ),
-                        ]),
+                          );
+                        },
                       ),
-                    );
-                  },
+                  ],
                 ),
-            ],
-          ),
               ),
             ),
           ),
         ],
       ),
     );
-    
-    
-    
-    
-    
-    
-
   }
 }
 
